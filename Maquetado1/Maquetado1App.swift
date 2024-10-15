4f648587259556db4c1bd44968578ec5755654e9
MiguelCoba0502 <miguel_8090@hotmail.com>
2024-10-15 06:22:10 -0600

    Initial Commit

---
+++ Maquetado1/Maquetado1App.swift
@@ -0,0 +1,17 @@
+//
+//  Maquetado1App.swift
+//  Maquetado1
+//
+//  Created by Eduardo Coba on 15/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Maquetado1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
